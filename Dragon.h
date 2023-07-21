52d0d00a712b9530d111caddf08d54c443387b81
GraceHanJiarui <102732338+GraceHanJiarui@users.noreply.github.com>
2023-07-21 18:53:09 -0400

    Add files via upload

--- Dragon.h
+++ Dragon.h
@@ -1,11 +1,10 @@
 #include "Enemy.h"
-#include "DragonHoard.h"
 
 class Dragon : Enemy {
     DragonHoard *hoard;
 public:
-    Dragon(int row, int col, string nameNotion = "Dragon", int currentHP = 150, int maxHP = 150, int atk = 20, int def = 20, DragonHoard *hoard);
+    Dragon(int row, int col, string nameNotion = "Dragon", int currentHP = 150, int maxHP = 150, int atk = 20, int def = 20, DragonHoard *hoard = nullptr);
     ~Dragon();
     void attackNotify() override;
-    void moveDecision(string behaviour) override;
+    void moveDecision() override;
 };

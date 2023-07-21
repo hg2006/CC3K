4bb52436f6591ff8f5910191109a9d3c7fee68ea
ChunxinZheng <115291818+ChunxinZheng@users.noreply.github.com>
2023-07-21 16:39:46 -0400

    Create Game.h

---
+++ Game.h
@@ -0,0 +1,21 @@
+#ifndef GAME
+#define GAME
+
+#include "cc3klib.h"
+#include "BuffedPlayer.h"
+
+struct Game {
+  private:
+    int level;
+    unique_ptr <BuffedPlayer> player;
+    vector <unique_ptr <Map>> maps;
+  public:
+    Game();
+    void UpdateGame ();
+    void LevelUp ();
+    void Restart ();
+    void GameOver ();
+    void ScoreBoard ();
+}
+
+#endif

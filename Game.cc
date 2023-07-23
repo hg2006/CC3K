b0e2a37b14917ae16cc1376eb4f251aced0134cb
ChunxinZheng <115291818+ChunxinZheng@users.noreply.github.com>
2023-07-23 09:41:59 -0400

    Create Game.cc

---
+++ Game.cc
@@ -0,0 +1,7 @@
+#include "Game.h"
+
+using namespace std;
+
+Game::Game(string nameNotion, int currentHP, int maxHP, int Atk, int Def, int gold):
+  level{1}, player { make_unique <BuffedPlayer> (nameNotion, currentHP, maxHP, Atk, Def, gold)},
+  maps {}
